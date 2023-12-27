b2468bbecd448a3a76a54a243c2aedb1ef59630b
NataliaFrancisca <nathifrancisca@gmail.com>
2023-12-27 19:40:15 -0300

    feat: change the bg color; create if statement to check if the user is clicking on the same button

--- game.py
+++ game.py
@@ -7,31 +7,31 @@ class Game():
     
     matrix_game = None;
     frame = None;
-    board_frame = None;
     
     currentPlayer = None;
     gameIsOver = None;
     
     def __init__(self):
+        self.root.title('JOGO DA VELHA COM PYHTON')
+
         self.matrix_game = [[None, None, None], [None, None, None], [None, None, None]];
         self.currentPlayer = "X";
         self.gameIsOver = False;
         
-        self.frame = tk.Frame(self.root, height=500, width=500, background='Light Blue');
-        self.frame['borderwidth'] = 2
+        self.frame = tk.Frame(self.root, background='Light Green');
         self.frame.pack();
     
         self.welcomePage();
     
         
     def welcomePage(self):
-        view = tk.Frame(self.frame, height=400, width=400, background='Light Blue');
+        view = tk.Frame(self.frame, height=400, width=400, background='Light Green');
         
-        titulo = tk.Label(view, text="JOGO DA VELHA COM PYTHON", font=('Lucida Sans', '16', 'bold'), background='Light Blue');
+        titulo = tk.Label(view, text="JOGO DA VELHA COM PYTHON", font=('Lucida Sans', '16', 'bold'), background='Light Green');
         titulo.pack(padx=(100, 100));
         titulo.pack(pady=(20, 0));
 
-        button = tk.Button(view, text='START', font=('Lucida Sans', '16', 'bold'), background='Yellow', command=lambda: self.startGame(view));
+        button = tk.Button(view, text='START', font=('Lucida Sans', '16', 'bold'), background='Pink', command=lambda: self.startGame(view));
         button.pack(padx=(100,100))
         button.pack(pady=(100, 20)); 
         
@@ -42,34 +42,35 @@ class Game():
         self.createButtons();
         
     def createButtons(self):
-        self.board_frame = tk.Frame(self.frame, height=200, width=200, background='Light Green');
+        board_frame = tk.Frame(self.frame);
         
         for i in range(3):
             for j in range(3):
-                self.matrix_game[i][j] = tk.Button(self.board_frame, text="", font=('Lucida Sans', '28', 'bold'), width=8, height=3, background="Yellow", 
-                                             command=lambda row=i, col=j: self.onClickButton(row, col))
+                self.matrix_game[i][j] = tk.Button(board_frame, text="", font=('Lucida Sans', '28', 'bold'), width=8, height=3, background="Pink", 
+                                            command=lambda row=i, col=j: self.onClickButton(row, col))
                 self.matrix_game[i][j].grid(row=i, column=j)
 
-        self.board_frame.pack(padx=(50, 50));
-        self.board_frame.pack(pady=(50, 50));
+        board_frame.pack(padx=(50, 50));
+        board_frame.pack(pady=(50, 50));
     
     def onClickButton(self, row, col):
+        # check if the user clicked in a button that was already clicked
+        if(self.matrix_game[row][col]['text'] != ""):
+            return False;
+            
         if(self.gameIsOver == False and self.matrix_game[row][col]['text'] == ""):
             self.matrix_game[row][col]['text'] = self.currentPlayer;
-
+        
         if self.checkIsWinner(row, col):
-            messagebox.showinfo("Jogo da Velha", f"Jogador {self.currentPlayer} venceu!");
+            messagebox.showinfo("JOGO DA VELHA COM PYHTON", f"JOGADOR {self.currentPlayer} VENCEU!", icon=('info'));
             self.finishGame();
         elif self.checkIsDraw():
-            messagebox.showinfo("Jogo da Velha", "O jogo terminou em empate!");
-            self.gameFinished();
+            messagebox.showinfo("JOGO DA VELHA COM PYHTON", "O JOGO TERMINOU EM EMPATE");
+            self.finishGame();
         else:
             self.switchPlayer();
     
-    
     def checkIsWinner(self, row, col):
-        for x in range(3):
-            print(self.matrix_game[x][col]);
 
         # check row
         if all(self.matrix_game[row][i]["text"] == self.currentPlayer for i in range(3)):
